c26089596c5c0b10818d3f7568b113e5ef5e1a6f
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-09-07 08:31:40 -0300

    começando a popular banco

--- backend/dscatalog/dscatalog/src/main/java/com/api/dscatalog/services/CategoryService.java
+++ backend/dscatalog/dscatalog/src/main/java/com/api/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.api.dscatalog.entities.Category;
 import com.api.dscatalog.repositories.CategoryRepository;
@@ -21,6 +22,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional (readOnly = true) // garante mais estabilidade banco 
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
